e6a20c78be71152a065e9d54cf4de9b6ea74fc34
idith almog <almog.idith@gmail.com>
2020-11-08 14:02:43 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -56,7 +56,7 @@ class Game
      end
      def turn
        input=self.current_player.move(@board)
-    
+
        until @board.valid_move?(input)
          input=self.current_player.move(@board)
        end
